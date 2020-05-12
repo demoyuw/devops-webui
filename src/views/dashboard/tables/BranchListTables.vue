e5527c0f89c1815e0d2f866225128cb1a0f132f1
YuWei Chou <demoyuw@gmail.com>
2020-05-12 15:30:14 +0800

    當點擊table最後按鈕時，顯示功能選單列表。

--- src/views/dashboard/tables/BranchListTables.vue
+++ src/views/dashboard/tables/BranchListTables.vue
@@ -36,19 +36,55 @@
         -->
         <template v-slot:item="row">
           <tr>
-            <td>{{row.item.branchname}}</td>
-            <td>{{row.item.Thelastcommitmessage}}</td>
-            <td>{{row.item.Thelastcommittime}}</td>
+            <td>{{ row.item.branchname }}</td>
+            <td>{{ row.item.Thelastcommitmessage }}</td>
+            <td>{{ row.item.Thelastcommittime }}</td>
             <td>
-                <v-btn
-                  class="mx-2"
-                  fab
-                  small
-                  color="grey"
-                  @click="onButtonClick(row.item)"
-                >
-                  <v-icon>mdi-hammer-wrench</v-icon>
-                </v-btn>
+              <!--
+              <v-btn
+                class="mx-2"
+                fab
+                small
+                color="grey"
+                @click="onButtonClick(row.item)"
+              >
+                <v-icon>mdi-hammer-wrench</v-icon>
+              </v-btn>
+              -->
+              <!--
+              <v-overflow-btn
+                class="my-2"
+                :items="items"
+                label="Overflow Btn - Dense"
+                dense
+              >
+                <v-icon>mdi-hammer-wrench</v-icon>
+              </v-overflow-btn>
+              -->
+              <v-menu
+                top
+                offset-x
+              >
+                <template v-slot:activator="{ on }">
+                  <v-btn
+                    color="primary"
+                    dark
+                    v-on="on"
+                  >
+                    <v-icon>mdi-hammer-wrench</v-icon>
+                  </v-btn>
+                </template>
+
+                <v-list>
+                  <v-list-item
+                    v-for="(item, index) in items"
+                    :key="index"
+                    @click="onButtonClick(on.item)"
+                  >
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
             </td>
           </tr>
         </template>
@@ -74,6 +110,7 @@
           },
           { text: '最新Commit訊息', value: 'Thelastcommitmessage' },
           { text: '最近一次Commit時間', value: 'Thelastcommittime' },
+          { text: '', value: '' },
         ],
         desserts: [
           {
@@ -91,6 +128,18 @@
             Thelastcommitmessage: 'Release v2.8.0',
             Thelastcommittime: '2 weeks ago',
           },
+        ],
+        items: [
+          { title: '新增檔案' },
+          { title: '上傳檔案' },
+          { title: '建立新資料夾' },
+          { title: '編輯檔案' },
+          { title: '新增分支' },
+          { title: '新增版本號' },
+          { title: '分支版控紀錄' },
+          { title: '下載檔案' },
+          { title: '合併分支' },
+          { title: '刪除分支' },
         ],
       }
     },
